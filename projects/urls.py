1e11284af6dcc27b180688849e96e7408acef195
Dharshan4038 <dharshand9@gmail.com>
2023-11-07 21:45:50 +0530

    final commit

--- projects/urls.py
+++ projects/urls.py
@@ -4,6 +4,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.mainPage, name="main"),
-    path('projects/', views.projects, name="projects")
+    path('main/', views.mainPage, name="main"),
+    path('login/', views.LoginPage, name='login'),
+    path('', views.SignupPage, name='signup'),
+    path('projects/', views.projects, name="projects"),
+    path('logout/', views.LogoutPage, name='logout')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- projects/views.py
+++ projects/views.py
@@ -1,23 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-# from .forms import FileForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
 import pandas as pd
 from . import training
 
+
 # Create your views here.
-
-
 def projects(request):
+    finalOutput = ''
+    reqInputs = ''
     if request.method == "POST":
         file = request.FILES['myFile']
-        reqInputs = request.POST.get('reqInputs')
+        reqInputs = request.POST.get('reqInputs', '')
         userInputs = reqInputs.split(',')
         finalOutput = training.makePrediction(file, userInputs)
-        context = {'finalOutput': finalOutput}
-        return render(request, 'predict.html', context)
+
+    context = {'finalOutput': finalOutput,
+               'reqInputs': reqInputs}
+    return render(request, 'predict.html', context)
 
     return render(request, 'predict.html')
 
 
+def LoginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        pass1 = request.POST.get('pass')
+        user = authenticate(request, username=username, password=pass1)
+        if user is not None:
+            login(request, user)
+            return redirect('main')
+        else:
+            return HttpResponse("Username or Password is incorrect!!!")
+
+    return render(request, 'login.html')
+
+
+def SignupPage(request):
+    if request.method == 'POST':
+        uname = request.POST.get('username')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('password1')
+        pass2 = request.POST.get('password2')
+
+        if pass1 != pass2:
+            return HttpResponse("Your password and confirm password are not Same!!")
+        else:
+            my_user = User.objects.create_user(uname, email, pass1)
+            my_user.save()
+            return redirect('login')
+
+    return render(request, 'signup.html')
+
+
+def LogoutPage(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
 def mainPage(request):
     return render(request, 'home.html')
